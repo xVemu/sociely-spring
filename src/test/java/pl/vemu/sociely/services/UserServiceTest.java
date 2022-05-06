a6e1730ad21332dcf9858d5ecbf65039d3d6b574
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2022-05-06 15:10:12 +0200

    add tests for patch and delete

--- src/test/java/pl/vemu/sociely/services/UserServiceTest.java
+++ src/test/java/pl/vemu/sociely/services/UserServiceTest.java
@@ -22,11 +22,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -58,7 +58,9 @@ class UserServiceTest {
     @Test
     void getAll() {
         when(repository.findAll(nullable(Pageable.class))).thenReturn(new PageImpl<>(users));
+
         var response = service.getAll(null);
+
         assertEquals(users.stream().map(mapper::toUserDto).toList(), response.getContent());
     }
 
@@ -66,8 +68,11 @@ class UserServiceTest {
     void getById() throws UserByIdNotFound {
         when(repository.findById(anyLong())).then(invocation -> users.stream().filter(
                 user -> user.getId().equals(invocation.getArgument(0))).findFirst());
+
         assertThrows(UserByIdNotFound.class, () -> service.getById(-123L), "User with id -123 not found!");
+
         var response = service.getById(users.get(0).getId());
+
         assertEquals(mapper.toUserDto(users.get(0)), response);
     }
 
@@ -79,6 +84,7 @@ class UserServiceTest {
             users.add(invocation.getArgument(0));
             return invocation.getArgument(0);
         });
+
         var userToSave =
                 new UserDtoRequest("Krazy", "Eight", users.get(0).getEmail(), "8vjfc4cp2chifwj");
         assertThrows(UserWithEmailAlreadyExist.class, () -> service.add(userToSave),
@@ -86,6 +92,7 @@ class UserServiceTest {
         );
         userToSave.setEmail("elicia.johnson@hotmail.com");
         var response = service.add(userToSave);
+
         verify(repository).findByEmail(userToSave.getEmail());
         verify(repository).save(any(User.class));
         assertEquals(userToSave.getEmail(), response.email());
@@ -95,10 +102,51 @@ class UserServiceTest {
     }
 
     @Test
-    void updateUser() {
+    void updateUser() throws UserByIdNotFound, UserWithEmailAlreadyExist {
+        when(repository.findById(anyLong())).then(invocation -> users.stream().filter(
+                user -> user.getId().equals(invocation.getArgument(0))).findFirst());
+        when(repository.findByEmail(anyString())).then(invocation -> users.stream().filter(
+                user -> user.getEmail().equals(invocation.getArgument(0))).findFirst());
+        when(repository.save(any(User.class))).then(invocation -> {
+            int index = IntStream.range(0, users.size())
+                    .filter(i -> users.get(i).getId().equals(
+                            ((User) invocation.getArgument(0)).getId())).findFirst().getAsInt();
+            users.set(index, invocation.getArgument(0));
+            return invocation.getArgument(0);
+        });
+
+        var userToUpdate =
+                new UserDtoRequest("Krazy", "Eight", users.get(2).getEmail(), "8vjfc4cp2chifwj");
+
+        assertThrows(
+                UserByIdNotFound.class, () -> service.updateUser(-123L, userToUpdate), "User with id -123 not found!");
+        assertThrows(
+                UserWithEmailAlreadyExist.class, () -> service.updateUser(0L, userToUpdate),
+                "User with email " + users.get(2).getEmail() + " already exist!"
+        );
+        userToUpdate.setEmail("jed.carroll@yahoo.com");
+        var response = service.updateUser(0L, userToUpdate);
+        verify(repository).findByEmail(userToUpdate.getEmail());
+        verify(repository).save(any(User.class));
+        assertEquals(userToUpdate.getEmail(), response.email());
+        assertEquals(userToUpdate.getName(), response.name());
+        assertEquals(userToUpdate.getSurname(), response.surname());
+        assertTrue(encoder.matches(userToUpdate.getPassword(), users.get(0).getPassword()));
+
     }
 
     @Test
-    void delete() {
+    void delete() throws UserByIdNotFound {
+        when(repository.findById(anyLong())).then(invocation -> users.stream().filter(
+                user -> user.getId().equals(invocation.getArgument(0))).findFirst());
+        doAnswer(invocation -> users.removeIf(user -> user.getId().equals(invocation.getArgument(0)))).when(
+                repository).deleteById(anyLong());
+
+        var userToDelete = users.get(0);
+        service.delete(userToDelete.getId());
+
+        assertThrows(UserByIdNotFound.class, () -> service.delete(-123L), "User with id -123 not found!");
+        assertEquals(2, users.size());
+        assertFalse(users.contains(userToDelete));
     }
 }
